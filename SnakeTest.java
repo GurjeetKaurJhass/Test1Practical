5da8c613005c4bc2402353b9612092bd64d78a27
gurjeet <gurjit.babrah@gmail.com>
2019-09-12 12:47:18 -0400

    Initial commit

---
+++ SnakeTest.java
@@ -0,0 +1,34 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SnakeTest {
+
+	@Before
+	public void setUp() throws Exception {
+		Snake s=new Snake("Peter",10,"coffee");
+		Snake s2=new Snake("Takis",80,"vegetables");
+		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+	
+	
+	String ExpectedOutput="vegetables";
+	String ActualOutput ="";
+	
+	
+		
+		
+		
+		fail("Not yet implemented");
+	}
+
+}
